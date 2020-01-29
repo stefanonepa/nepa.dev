efb2162b4ed68d1f3fcc97e27d5ac4eb13b60aa7
stefanovualto <stefano.nepa@vualto.com>
2020-01-29 22:19:18 +0000

    github pages

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,12 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: "nepa.dev",
+  siteUrl: "https://stefanonepa.github.io",
+  pathPrefix: "/nepa.dev",
   plugins: []
-}
+};
